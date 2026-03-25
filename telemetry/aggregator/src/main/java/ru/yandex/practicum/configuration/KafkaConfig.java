c22ef9c8b3720fdcbc5363d4e6eff3f1a8d32885
Konstantin <tenenbaum1994@gmail.com>
2026-03-25 18:37:03 +0700

    добавил KafkaConfig класс с настройками; исправил ошибку в application.yaml

---
+++ telemetry/aggregator/src/main/java/ru/yandex/practicum/configuration/KafkaConfig.java
@@ -0,0 +1,36 @@
+package ru.yandex.practicum.configuration;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import java.util.Properties;
+
+@Getter
+@Setter
+@ToString
+@ConfigurationProperties("aggregator.kafka")
+public class KafkaConfig {
+
+    private ProducerConfig producer;
+    private ConsumerConfig consumer;
+
+    @Getter
+    @Setter
+    @ToString
+    public static class ProducerConfig {
+
+        private String topic;
+        private Properties properties;
+    }
+
+    @Getter
+    @Setter
+    @ToString
+    public static class ConsumerConfig {
+
+        private String topic;
+        private Properties properties;
+    }
+}
