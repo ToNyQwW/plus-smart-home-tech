60ff6f0066e3596709830119fc8bc50162648326
Konstantin <tenenbaum1994@gmail.com>
2026-03-28 13:31:42 +0700

    добавлены сущности для работы с бд; добавлены интерфейсы репозиториев

---
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/dal/repository/ScenarioRepository.java
@@ -0,0 +1,14 @@
+package ru.yandex.practicum.dal.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ru.yandex.practicum.dal.entity.Scenario;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface ScenarioRepository extends JpaRepository<Scenario, Long> {
+
+    List<Scenario> findByHubId(String hubId);
+
+    Optional<Scenario> findByHubIdAndName(String hubId, String name);
+}
