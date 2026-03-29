c2a4a80968a375f3dc28c680e32e7992fa5bf5dc
Konstantin <tenenbaum1994@gmail.com>
2026-03-29 17:17:30 +0700

    Полная переработка логики приложения. В надежде что получится

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/HubEventStarter.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/HubEventStarter.java
@@ -1,4 +1,4 @@
-package ru.yandex.practicum.service;
+package ru.yandex.practicum;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -7,50 +7,35 @@ import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.configuration.KafkaConfig;
-import ru.yandex.practicum.kafka.telemetry.event.*;
-import ru.yandex.practicum.service.handler.HubEventHandler;
+import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
+import ru.yandex.practicum.service.HubEventService;
 
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 @Slf4j
 @Component
-public class HubEventProcessor implements Runnable {
+public class HubEventStarter implements Runnable {
 
+    private final HubEventService hubEventService;
     private final KafkaConfig.ConsumerConfig kafkaConfig;
     private final KafkaConsumer<String, HubEventAvro> consumer;
-    private final Map<String, HubEventHandler> hubEventHandlers;
 
-    public HubEventProcessor(KafkaConfig kafkaConfig, List<HubEventHandler> handlers) {
+    public HubEventStarter(KafkaConfig kafkaConfig, HubEventService hubEventService) {
+        this.hubEventService = hubEventService;
         this.kafkaConfig = kafkaConfig.getHubEventConsumer();
         this.consumer = new KafkaConsumer<>(kafkaConfig.getHubEventConsumer().getProperties());
-        this.hubEventHandlers = handlers.stream()
-                .collect(Collectors.toMap(
-                        HubEventHandler::getType,
-                        Function.identity()
-                ));
-
-        Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
     }
 
     @Override
     public void run() {
+        Runtime.getRuntime().addShutdownHook(new Thread(consumer::wakeup));
         try {
             consumer.subscribe(List.of(kafkaConfig.getTopic()));
-
             while (true) {
                 ConsumerRecords<String, HubEventAvro> records = consumer.poll(kafkaConfig.getPollTimeout());
 
-                if (records.isEmpty()) {
-                    continue;
-                }
-
                 for (ConsumerRecord<String, HubEventAvro> record : records) {
-                    Object payload = record.value().getPayload();
-                    HubEventHandler hubEventHandler = hubEventHandlers.get(payload.getClass());
-                    hubEventHandler.handle(record.key(), record.value());
+                    handleRecord(record);
                 }
                 consumer.commitSync();
             }
@@ -66,5 +51,9 @@ public class HubEventProcessor implements Runnable {
                 consumer.close();
             }
         }
+    }
+
+    private void handleRecord(ConsumerRecord<String, HubEventAvro> record) {
+        hubEventService.handle(record.value());
     }
 }
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/dal/entity/Scenario.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/dal/entity/Scenario.java
@@ -1,17 +1,21 @@
 package ru.yandex.practicum.dal.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
-import lombok.Setter;
+import lombok.NoArgsConstructor;
 
 import java.util.HashMap;
 import java.util.Map;
 
 @Entity
-@Getter @Setter
 @Table(name = "scenarios")
+@Getter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class Scenario {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -19,35 +23,26 @@ public class Scenario {
     @Column(name = "hub_id")
     private String hubId;
 
+    @Column(name = "name")
     private String name;
 
-    @OneToMany
-    @MapKeyColumn(
-            table = "scenario_conditions",
-            name = "sensor_id")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @MapKeyColumn(table = "scenario_conditions", name = "sensor_id")
     @JoinTable(
             name = "scenario_conditions",
             joinColumns = @JoinColumn(name = "scenario_id"),
-            inverseJoinColumns = @JoinColumn(name = "condition_id"))
+            inverseJoinColumns = @JoinColumn(name = "condition_id")
+    )
+    @Builder.Default
     private Map<String, Condition> conditions = new HashMap<>();
 
-    @OneToMany
-    @MapKeyColumn(
-            table = "scenario_actions",
-            name = "sensor_id")
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    @MapKeyColumn(table = "scenario_actions", name = "sensor_id")
     @JoinTable(
             name = "scenario_actions",
             joinColumns = @JoinColumn(name = "scenario_id"),
-            inverseJoinColumns = @JoinColumn(name = "action_id"))
+            inverseJoinColumns = @JoinColumn(name = "action_id")
+    )
+    @Builder.Default
     private Map<String, Action> actions = new HashMap<>();
-
-    @Transient
-    public void addCondition(String sensorId, Condition condition) {
-        this.conditions.put(sensorId, condition);
-    }
-
-    @Transient
-    public void addAction(String sensorId, Action action) {
-        this.actions.put(sensorId, action);
-    }
 }
