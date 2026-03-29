871627623b89d80ff05440f614d4b0dcb7b92d19
Konstantin <tenenbaum1994@gmail.com>
2026-03-29 15:48:43 +0700

    изменение метода process в SnapshotAnalyzer

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/service/SnapshotAnalyzer.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/service/SnapshotAnalyzer.java
@@ -5,13 +5,10 @@ import org.springframework.stereotype.Service;
 import ru.yandex.practicum.dal.entity.Condition;
 import ru.yandex.practicum.dal.entity.Scenario;
 import ru.yandex.practicum.dal.repository.ScenarioRepository;
-import ru.yandex.practicum.kafka.telemetry.event.ConditionOperationAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorStateAvro;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 import ru.yandex.practicum.service.handler.SensorEventHandler;
 
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
@@ -34,60 +31,36 @@ public class SnapshotAnalyzer {
     }
 
     public List<Scenario> process(SensorsSnapshotAvro snapshot) {
-        String hubId = snapshot.getHubId();
-        List<Scenario> scenarios = scenarioRepository.findByHubId(hubId);
+        return scenarioRepository
+                .findByHubId(snapshot.getHubId())
+                .stream()
+                .filter(scenario -> isConditionsMatchSnapshot(snapshot, scenario.getConditions()))
+                .toList();
+    }
 
-        if (scenarios.isEmpty()) {
-            log.info("Для хаба {} сценарии не найдены", hubId);
-            return Collections.emptyList();
+    private boolean isConditionsMatchSnapshot(SensorsSnapshotAvro snapshot, Map<String, Condition> conditions) {
+        return conditions
+                .entrySet()
+                .stream()
+                .allMatch(conditionEntry ->
+                        checkCondition(conditionEntry.getKey(), conditionEntry.getValue(), snapshot)
+                );
+    }
+
+    private boolean checkCondition(String sensorId, Condition condition, SensorsSnapshotAvro snapshot) {
+        if (!snapshot.getSensorsState().containsKey(sensorId)) {
+            return false;
         }
 
-        List<Scenario> result = new ArrayList<>();
+        SensorStateAvro sensorStateAvro = snapshot.getSensorsState().get(sensorId);
+        SensorEventHandler<?> sensorEventHandler = sensorEventHandlers.get(sensorStateAvro.getData().getClass());
 
-        for (Scenario scenario : scenarios) {
-            Map<String, Condition> conditions = scenario.getConditions();
+        Integer value = sensorEventHandler.getValue(condition.getType(), sensorStateAvro);
 
-            boolean allConditionsMet = true;
-
-            for (Map.Entry<String, Condition> entry : conditions.entrySet()) {
-                String sensorId = entry.getKey();
-                Condition condition = entry.getValue();
-
-                SensorStateAvro sensorState = snapshot.getSensorsState().get(sensorId);
-
-                if (sensorState == null) {
-                    log.warn("Состояние сенсора {} отсутствует в снапшоте", sensorId);
-                    allConditionsMet = false;
-                    break;
-                }
-
-                SensorEventHandler<?> handler = sensorEventHandlers.get(sensorState.getData().getClass());
-                if (handler == null) {
-                    log.warn("Нет обработчика для типа сенсора {}", sensorState.getData().getClass());
-                    allConditionsMet = false;
-                    break;
-                }
-
-                Integer sensorValue = handler.getValue(condition.getType(), sensorState);
-
-                ConditionOperationAvro operation = condition.getOperation();
-                boolean conditionMet = switch (operation) {
-                    case EQUALS -> sensorValue.equals(condition.getValue());
-                    case LOWER_THAN -> sensorValue < condition.getValue();
-                    case GREATER_THAN -> sensorValue > condition.getValue();
-                };
-
-                if (!conditionMet) {
-                    allConditionsMet = false;
-                    break;
-                }
-            }
-
-            if (allConditionsMet) {
-                result.add(scenario);
-            }
-        }
-
-        return result;
+        return switch (condition.getOperation()) {
+            case EQUALS -> value.equals(condition.getValue());
+            case LOWER_THAN -> value < condition.getValue();
+            case GREATER_THAN -> value > condition.getValue();
+        };
     }
 }
