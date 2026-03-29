baf18d1a7c058f693ad44a70f35bfed24262f3a2
Konstantin <tenenbaum1994@gmail.com>
2026-03-29 17:37:14 +0700

    добавление оффсетов при работе с кайфкой

--- telemetry/analyzer/src/main/java/ru/yandex/practicum/HubEventStarter.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/HubEventStarter.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.configuration.KafkaConfig;
 import ru.yandex.practicum.kafka.telemetry.event.HubEventAvro;
 import ru.yandex.practicum.service.HubEventService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -19,6 +23,7 @@ public class HubEventStarter implements Runnable {
     private final HubEventService hubEventService;
     private final KafkaConfig.ConsumerConfig kafkaConfig;
     private final KafkaConsumer<String, HubEventAvro> consumer;
+    private final Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
 
     public HubEventStarter(KafkaConfig kafkaConfig, HubEventService hubEventService) {
         this.hubEventService = hubEventService;
@@ -34,10 +39,13 @@ public class HubEventStarter implements Runnable {
             while (true) {
                 ConsumerRecords<String, HubEventAvro> records = consumer.poll(kafkaConfig.getPollTimeout());
 
+                int count = 0;
                 for (ConsumerRecord<String, HubEventAvro> record : records) {
                     handleRecord(record);
+                    manageOffsets(record, count, consumer);
+                    count++;
                 }
-                consumer.commitSync();
+                consumer.commitAsync();
             }
 
         } catch (WakeupException ignored) {
@@ -46,10 +54,26 @@ public class HubEventStarter implements Runnable {
         } finally {
 
             try {
-                consumer.commitSync();
+                consumer.commitSync(currentOffsets);
             } finally {
                 consumer.close();
             }
+        }
+    }
+
+    private void manageOffsets(ConsumerRecord<String, HubEventAvro> record, int count,
+                               KafkaConsumer<String, HubEventAvro> consumer) {
+        currentOffsets.put(
+                new TopicPartition(record.topic(), record.partition()),
+                new OffsetAndMetadata(record.offset() + 1)
+        );
+
+        if (count % 10 == 0) {
+            consumer.commitAsync(currentOffsets, (offsets, exception) -> {
+                if (exception != null) {
+                    log.warn("Ошибка во время фиксации оффсетов: {}", offsets, exception);
+                }
+            });
         }
     }
 
--- telemetry/analyzer/src/main/java/ru/yandex/practicum/SnapshotStarter.java
+++ telemetry/analyzer/src/main/java/ru/yandex/practicum/SnapshotStarter.java
@@ -4,13 +4,17 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.springframework.stereotype.Component;
 import ru.yandex.practicum.configuration.KafkaConfig;
 import ru.yandex.practicum.kafka.telemetry.event.SensorsSnapshotAvro;
 import ru.yandex.practicum.service.SnapshotService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j
 @Component
@@ -19,6 +23,7 @@ public class SnapshotStarter {
     private final SnapshotService snapshotService;
     private final KafkaConfig.ConsumerConfig kafkaConfig;
     private final KafkaConsumer<String, SensorsSnapshotAvro> consumer;
+    private final Map<TopicPartition, OffsetAndMetadata> currentOffsets = new HashMap<>();
 
     public SnapshotStarter(KafkaConfig kafkaConfig, SnapshotService snapshotService) {
         this.snapshotService = snapshotService;
@@ -34,10 +39,13 @@ public class SnapshotStarter {
             while (true) {
                 ConsumerRecords<String, SensorsSnapshotAvro> records = consumer.poll(kafkaConfig.getPollTimeout());
 
+                int count = 0;
                 for (ConsumerRecord<String, SensorsSnapshotAvro> record : records) {
                     handleRecord(record);
+                    manageOffsets(record, count, consumer);
+                    count++;
                 }
-                consumer.commitSync();
+                consumer.commitAsync();
             }
 
         } catch (WakeupException ignored) {
@@ -46,10 +54,26 @@ public class SnapshotStarter {
         } finally {
 
             try {
-                consumer.commitSync();
+                consumer.commitSync(currentOffsets);
             } finally {
                 consumer.close();
             }
+        }
+    }
+
+    private void manageOffsets(ConsumerRecord<String, SensorsSnapshotAvro> record, int count,
+                               KafkaConsumer<String, SensorsSnapshotAvro> consumer) {
+        currentOffsets.put(
+                new TopicPartition(record.topic(), record.partition()),
+                new OffsetAndMetadata(record.offset() + 1)
+        );
+
+        if (count % 10 == 0) {
+            consumer.commitAsync(currentOffsets, (offsets, exception) -> {
+                if (exception != null) {
+                    log.warn("Ошибка во время фиксации оффсетов: {}", offsets, exception);
+                }
+            });
         }
     }
 
