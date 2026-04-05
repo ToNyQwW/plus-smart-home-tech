0f5dc4ec4ae311fb66f10d12ed52c97f7a3169a1
Konstantin <tenenbaum1994@gmail.com>
2026-04-05 11:24:34 +0700

    Начало работы с модулем shopping-store: добавлены зависимости в pom.xml создан main класс runner

---
+++ commerce/shopping-store/src/main/java/ru/yandex/practicum/controller/ShoppingStoreController.java
@@ -0,0 +1,11 @@
+package ru.yandex.practicum.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/shopping-store")
+public class ShoppingStoreController {
+
+
+}
